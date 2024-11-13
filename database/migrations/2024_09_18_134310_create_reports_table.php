6e3f7f0e4c123b083470063a528487a59b274655
nyenyel <154278884+nyenyel@users.noreply.github.com>
2024-11-14 01:11:24 +0800

    rev

--- database/migrations/2024_09_18_134310_create_reports_table.php
+++ database/migrations/2024_09_18_134310_create_reports_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -17,7 +18,7 @@ return new class extends Migration
             $table->string('desc');
             $table->string('reporter_name');
             $table->string('evidence')->nullable();
-            $table->timestamp('date_time');
+            $table->timestamp('date_time')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->unsignedBigInteger('lib_status_id');
             $table->unsignedBigInteger('lib_category_id');
             $table->unsignedBigInteger('location_id');
